fe9f84ddcd9671869f69f12b78ee0fe3a01cf348
Lucy C <lucychen96@gmail.com>
2025-05-07 01:10:27 -0400

    Move 'Welcome...' message to index.erb so it shows up on the webpage

--- color_ranker_webpage.rb
+++ color_ranker_webpage.rb
@@ -2,7 +2,6 @@ require 'sinatra'
 require './color_ranker'
 
 get '/' do
-    'Welcome to Color Ranker where you can rank ugly colors!'
     @hex_color = generate_random_hex_color
     erb :index
 end
