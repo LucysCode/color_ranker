9fe9c189cb16d5a43341c3f68a5389ff7726a978
Lucy C <lucychen96@gmail.com>
2025-05-07 01:41:42 -0400

    Add button to generate new color

--- color_ranker.rb
+++ color_ranker.rb
@@ -2,3 +2,4 @@ def generate_random_hex_color
     "#" + "%06x" % (rand * 0xffffff)
 end
 
+# utility file to keep this method modular and reusable
--- color_ranker_webpage.rb
+++ color_ranker_webpage.rb
@@ -1,8 +1,18 @@
+# app logic, routes, and controllers
+
 require 'sinatra'
 require './color_ranker'
+require 'json' 
 
+# Route to render main page with random color
 get '/' do
     @hex_color = generate_random_hex_color
     erb :index
+end
+
+# Route to return new random color when button is clicked
+get '/new_color' do
+    content_type :json
+    { hex_color: generate_random_hex_color }.to_json
 end
 
