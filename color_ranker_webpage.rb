719abb1dd78d81160cd24a2f5357ede9e0028937
Lucy C <lucychen96@gmail.com>
2025-05-07 21:32:53 -0400

    Remove unnecessary files

--- color_ranker_webpage.rb
+++ color_ranker_webpage.rb
@@ -2,12 +2,14 @@
 
 require 'sinatra'
 require './color_ranker'
-require 'json' 
+
+set :public_folder, 'public'
 
 # Route to render main page with random color
 get '/' do
     @hex_color = generate_random_hex_color
     erb :index
+    # send_file File.join(settings.public_folder, 'index.html')
 end
 
 # Route to return new random color when button is clicked
